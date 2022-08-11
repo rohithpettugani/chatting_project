a44b03fa90590d2d659966ff80a91ae4dc3a0f92
rohith <rohith.pettugani@dataunveil.com>
2022-08-11 12:37:33 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+
+app.config['SECRET'] = "rohith"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(message):
+    print("connected")
+    if message != "User connected!":
+        send(message, broadcast=True)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__  == "__main__":
+    socketio.run(app, host='localhost')
